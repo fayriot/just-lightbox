a65f0660fcb9dfa7570d41d1b32eb6aa044429b2
fayriot <fayriot@gmail.com>
2022-03-02 10:40:30 +0700

    fix build

--- just-lightbox/src/index.ts
+++ just-lightbox/src/index.ts
@@ -1,3 +1,6 @@
 export * from './just-lightbox/just-lightbox.module';
 export {JUST_LIGHTBOX_OPTIONS} from './just-lightbox/tokens/';
 export {justLightboxDefaults} from './just-lightbox/constants';
+export {LightboxComponent} from './just-lightbox/components/lightbox/lightbox.component';
+export {JustLightboxDirective} from './just-lightbox/directives/just-lightbox.directive';
+export {JustLightboxGesturesDirective} from './just-lightbox/directives/just-lightbox-gestures.directive';
